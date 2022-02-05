bdffc275c732c76a7601d4d1b6bd6b32a6f205ef
CudiLala <augustinemadu9@gmail.com>
2022-02-05 13:51:40 +0100

    fixing errors

--- components/Forms/index.tsx
+++ components/Forms/index.tsx
@@ -6,7 +6,7 @@ export const QuizSearchForm: React.FC = () => {
   return (
     <form className={styles.QuizSearchForm}>
       <Box _className={styles.InputContainer} size={[0, 0, 4]}>
-        <label>Search:</label>
+        <label style={{ fontSize: "1rem" }}>Search:</label>
         <Box _className={styles.InputBox} size={[1, 0, 0]}>
           <input type="text" />
         </Box>
