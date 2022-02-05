a65108a54e4194f2f23f7984fb9b1120baadd718
CudiLala <augustinemadu9@gmail.com>
2022-02-05 19:29:32 +0100

    fixing errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,9 @@ import { QuizSearchForm } from "components/Forms";
 const Home: NextPageWithLayout = () => {
   return (
     <div className="content-width">
-      <div>
-        <p>
-          Hi, Welcome to Quizr. Test your knowledge on some mind blowing facts
-          you may or may not know about. To get started, choose one of the
-          following quizes below. You can search for quizes or sort based on
-          category, difficulty or popularity.
-        </p>
-      </div>
+      <Box size={[8, 8, 4]}>
+        <p>Search</p>
+      </Box>
       <Box size={[4, 8]}>
         <QuizSearchForm />
       </Box>
