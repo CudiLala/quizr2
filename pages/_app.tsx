66ed109fe81d4adf60b9e76b89fffdbf583a888d
CudiLala <augustinemadu9@gmail.com>
2022-02-05 13:36:30 +0100

    fixing errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,11 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           Quizr: Test your knowledge on the most common facts about stuff
         </title>
         <link rel="icon" href="/logo.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta charSet="utf-8" />
       </Head>
       <AppWrapper>
         <Layout>
