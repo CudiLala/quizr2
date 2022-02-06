97d53ad3006367f1e9c5aecdbecf4445a81ad231
CudiLala <augustinemadu9@gmail.com>
2022-02-06 12:54:06 +0100

    implemented sorter in search form

--- components/Forms/index.tsx
+++ components/Forms/index.tsx
@@ -1,6 +1,17 @@
 import Box from "components/Boxes";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { DefaultProps } from "types/app";
 import styles from "./Forms.module.css";
+
+interface PropOptions extends DefaultProps {
+  name: string;
+  selected?: boolean;
+  setSelected?: React.Dispatch<React.SetStateAction<Selection>>;
+}
+interface Selection {
+  name: string;
+  mode: "ascending" | "descending";
+}
 
 export const QuizSearchForm: React.FC = () => {
   return (
@@ -14,9 +25,9 @@ export const QuizSearchForm: React.FC = () => {
       <Box size={[0, 0, 4]} _className={styles.InputContainer}>
         <label>Sort By:</label>
         <Selection>
-          <Options>Difficulty</Options>
-          <Options>Popularity</Options>
-          <Options>A-Z</Options>
+          <Options name="popularity">Popularity</Options>
+          <Options name="difficulty">Difficulty</Options>
+          <Options name="alphabetically">A-Z</Options>
         </Selection>
       </Box>
     </form>
@@ -24,8 +35,48 @@ export const QuizSearchForm: React.FC = () => {
 };
 
 const Selection: React.FC = ({ children }) => {
-  return <div className={styles.Selection}>{children}</div>;
+  const [selected, setSelected] = useState<Selection>();
+
+  /* eslint-disable */
+  useEffect(() => {
+    React.Children.forEach(
+      children,
+      (child, idx) =>
+        //@ts-ignore
+        idx === 0 && setSelected({ name: child?.props.name, mode: "ascending" })
+    );
+  }, []);
+  /*eslint-enable */
+
+  return (
+    <div className={styles.Selection}>
+      {React.Children.map(children, (child, idx) => {
+        //@ts-ignore
+        const option = React.cloneElement(child, {
+          setSelected,
+          //@ts-ignore
+          selected: selected?.name === child?.props.name,
+        });
+        return option;
+      })}
+    </div>
+  );
 };
-const Options: React.FC = ({ children }) => {
-  return <div className={styles.Options}>{children}</div>;
+
+const Options: React.FC<PropOptions> = ({
+  children,
+  name,
+  selected,
+  setSelected,
+}) => {
+  return (
+    <div
+      className={`${styles.Options} ${selected ? styles.Selected : ""}`}
+      onClick={() =>
+        !!setSelected && setSelected({ name: name, mode: "ascending" })
+      }
+    >
+      {children}
+    </div>
+  );
 };
